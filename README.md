1245586471dcf1a84ed40c8b138168b797a32e67
Victor Bolanle <bolanlevictoroladimeji@gmail.com>
2026-03-26 12:13:03 +0100

    javascript assignment

---
+++ README.md
@@ -0,0 +1,2 @@
+"# javascript_assignment_1" 
+"# javascript_assignment_1" 
---
+++ profile.js
@@ -0,0 +1,41 @@
+// Declare Variables
+
+let userName = "Victor"
+let age = 30;
+let isStudent = true;
+let email = "bvoladimeji@gmail.com";
+let hobbies = ["hiking", "coding", "cycling"];
+console.log(userName)
+console.log(age)
+console.log(isStudent)
+console.log(email)
+console.log(hobbies)
+
+// typeof
+
+console.log(typeof userName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof email);
+console.log(typeof hobbies);
+
+
+//type coercion
+
+let number = "5";
+let result = "5" + 2;
+let correctResult = 5 + 2;
+console.log(correctResult);
+
+//template literals
+
+let profile = `My name is ${userName}, I am ${age} years old, my hobbies is ${hobbies [1]}.`;
+console.log(profile);
+
+
+
+
+
+
+
+
